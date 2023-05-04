57c6a85f4e34baae94d36238f7a236f57a3e38ec
swaraj-jain <happy2000jain@gmail.com>
2023-05-04 17:39:26 +0530

    changed modified

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-const users   = require("../models/user.js");
+const Users   = require("../models/user.js");
 const jwt = require("jsonwebtoken");
 const bcrypt  = require('bcrypt');
 
@@ -6,19 +6,19 @@ const saltRounds = 10;
 const JWT_SECRET = "newtonSchool";
 
 /*
+loginUser Controller
 
 Get request json file structure
     obj =  {
-        "email":email,
-        "password": password,
+        email:email,
+        password: password,
     }
 
 
 You need to complete the controller for user loginUser.
 you need to login the user.
 Complete the schema and to look the user schema look ../models/user.js
-password is hashed using bcrypt while saving it.
-
+password is hashed using bcrypt saving it.
 
 
 Response on different scenario
@@ -42,7 +42,8 @@ json = {
 
 3. Success Login
 
-generate a JSON web token (JWT) with the user's { userId, name, email, role } as the payload,
+//JWT token that will contain payload containing { userId }
+generate a JSON web token (JWT) with the user's { userId } as the payload,
 sign it with a JWT_SECRET key, and set the expiration time to 1 hour
 //Don't change JWT_SECRET Secret Key.
 
@@ -61,7 +62,7 @@ const loginUser =async (req, res) => {
     const email  = req.body.email;
     const password = req.body.password;
 
-    //Write you code here.
+    //Write your code here.
 
 }
 
@@ -71,15 +72,16 @@ const loginUser =async (req, res) => {
 
 Post request json file structure
     obj =  {
-        "name":name,
-        "email":email,
-        "password": password,
-        "role": role
+        name:name,
+        email:email,
+        password: password,
+        role: role
     }
 
 
 You need to complete the controller for user signupUser.
 you need to register the user.
+If any user with given mail allready exist than throw error.
 Complete the schema and to look the user schema look ../models/user.js
 you should hash the password using bcrypt before saving it.
 
@@ -95,21 +97,31 @@ json = {
     "status": 'success'
 }
 
-2. Will get error if email all ready exist.
+2. if user with given email all ready exist.
+
+409 Status code with 
+json = {
+    "status": 'fail',
+    "message": 'User with given Email allready register'.
+}
+
+3. if something went wrong
 
 404 Status code with 
 json = {
     "status": 'fail',
-    "message": error message we got.
+    "message": 'Something went wrong'
 }
 
 */
 
 const signupUser = async (req, res) => {
 
-    var {email, password, name, role} = req.body;
-   //Write your code here.
+    const {email, password, name, role} = req.body;
+
+     //Write your code here.
 
 }
 
 module.exports = { loginUser , signupUser };
+
